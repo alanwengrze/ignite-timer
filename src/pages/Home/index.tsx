abd4c8701badda4d222caf584f40de9c616317dc
alan <alanwengrzee@gmail.com>
2024-09-20 20:05:30 -0300

    :books: docs: add docs from component Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -14,12 +14,14 @@ import {
 } from "./styles";
 import { useState } from "react";
 
+// Create a new zod schema
 const newCycleFormValidationSchema = z.object({
   task: z.string().min(1, 'Informe a tarefa'),
   minutesAmount: z.number().min(5).max(60),
   owner: z.string().optional()
 })
 
+// Create a new zod type (infer get the type of the schema)
 type NewCycleFormData = z.infer<typeof newCycleFormValidationSchema>
 
 interface Cycle{
@@ -41,22 +43,31 @@ export function Home(){
     }
   });
 
+  // Create a new cycle (submit)
   function handleCreateNewCycle(data: NewCycleFormData) {
+    // create a cycle (like a Cycle interface) based on the data
     const newCycle: Cycle = {
       id: String(new Date().getTime()),
       task: data.task,
       minutesAmount: data.minutesAmount
     }
 
+    // add the new cycle to the state
     setCycles((state) => [...state, newCycle])
+
+    // set the active cycle id
     setActiveCycleId(newCycle.id)
+
+    // reset the form
     reset()
   }
 
+  // get the active cycle
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
 
   console.log(activeCycle)
 
+  // watch for changes in the task input (controlled input)
   const task = watch('task')
   const isSubmitDisabled = !task
 
