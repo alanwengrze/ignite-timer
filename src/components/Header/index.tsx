8d1f8bec997c10e8f7c8445266cf64c02b8b8fa5
alan <alanwengrzee@gmail.com>
2024-11-17 14:39:18 -0300

    :bug: fix: remove logo svg import

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,9 @@
 import { HeaderContainer } from "./styles";
 import { Timer, Scroll } from "phosphor-react";
-import logoIgnite from "../../assets/logo.svg";
 import { NavLink } from "react-router-dom";
 export function Header(){
   return(
     <HeaderContainer>
-      <img src={logoIgnite} alt="Dois triângulos verdes sobrepostos" />
       <nav>
         <NavLink to="/" title="Timer"><Timer /></NavLink>
         <NavLink to="/history" title="Histórico"><Scroll /></NavLink>
